de1df4815de2e8d3a7058975852b82cf124a88af
skell94 <sven.kellenberger@kellenberger.tv>
2017-05-05 19:42:05 +0200

    word cloud works now

--- opendata_app/js/application.js
+++ opendata_app/js/application.js
@@ -3,57 +3,143 @@
 var displayedVotesStack = [];
 var displayedWordsWithIdsStack = [];
 
+// stores the word and ids pairs of all words. Is calculated initially and can later be reused.
+var calculatedWordsHash = {};
+
+// older browsers don't support the 'includes' function. In this case the function is defined separately
+if (!String.prototype.includes) {
+     String.prototype.includes = function(word) {
+         return this.search(word)!=-1;
+     };
+ }
+
 // loads the words for the wordcloud
 function loadWords(){
+  var badWords = ["januar", "februar", "märz", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "dezember",
+				"betreffend", "art", "bbl", "teil", "neu", "artikel", "mass", "der", "gegen", "pro", "schluss", "aug", "ein", "urs", "bei", "für", "nach", "mit", "gesetz", "beschluss"];
   var currentVotes = displayedVotesStack[displayedVotesStack.length-1];   // get the current votes
   var newWordList = [];
+  var wordHash = {};
   for(var i = 0; i<currentVotes.length; ++i){
     var vote = currentVotes[i];
-    var description = vote.description.replace(/["()«»:0-9-]/g, "");  // get the description and remove all specail characters except characters and dots
+    var description = vote.description.replace(/["()!«»\[\]:0-9-]/g, "");  // get the description and remove all specail characters except characters and dots
     var words = description.split(" ");
     for(var j=0; j<words.length; ++j){
       var word = words[j];
-      if(word == "" || word.length<3 ||(word.length<7 && word.charAt(0) === word.charAt(0).toLowerCase())){   // if one of these criteria is met, the word shouldn't be in the wordcloud
+      if(word == "" || word.indexOf(".")!=-1 || word.length<3 ||(word.length<7 && word[0] === word[0].toLowerCase())){   // if one of these criteria is met, the word shouldn't be in the wordcloud
         continue;
       }
-      var wordAdded = false;
-      for(var l=0; l<newWordList.length; ++l){
-        var currentWord = newWordList[l];
-        var returnWord = currentWord.compareWord(word, vote.id)
-        if(returnWord != null){
-          wordAdded = true;
-          if(!isWordAlreadyInList(newWordList, returnWord)){
-            newWordList.push(returnWord);
-          }
+      word = word.toLowerCase();
+      if(badWords.indexOf(word)!=-1){
+        continue;
+      }
+      if(!wordHash.hasOwnProperty(word)){
+        wordHash[word] = new Set();
+      }
+      wordHash[word].add(vote.id);
+    }
+  }
+  var hashKeys =Object.keys(wordHash);
+  var checkedWords = {};
+  for(var i=0; i<hashKeys.length; ++i){
+    var word = hashKeys[i];
+    if(!wordHash.hasOwnProperty(word)){   //continue if this word has already been deleted.
+      continue;
+    }
+    var ids = wordHash[word];
+    var toBeDeleted = false;
+    var updatedKeys = Object.keys(wordHash);
+    for(var j=0; j<updatedKeys.length; ++j){
+      var compareWord = updatedKeys[j];
+      if((checkedWords[compareWord] && !word.includes(compareWord)) || word.localeCompare(compareWord)==0){
+        continue;
+      } else if(compareWord.includes(word)){
+        wordHash[word] = new Set([...ids, ...wordHash[compareWord]]);
+        if(isSameWord(word, compareWord)){
+          delete wordHash[compareWord];
+        }
+      } else if(word.includes(compareWord)){
+        wordHash[compareWord] = new Set([...ids, ...wordHash[compareWord]]);
+        if(isSameWord(compareWord, word)){
+          toBeDeleted = true
         }
       }
-      if(!wordAdded){    // if the word is not already added to some wordWithIds object, create a new one
-        newWordList.push(new WordWithIds(word, vote.id));
-      }
+    }
+    if(toBeDeleted){
+      delete wordHash[word];
+    } else {
+      checkedWords[word]=true;
     }
   }
-  for(var i=0; i<newWordList.length; ++i){
-    console.log(newWordList[i].word+": "+newWordList[i].getCount());
-  }
-  console.log("Wörter: "+newWordList.length);
-  console.log("finished looping");
+  calculatedWordsHash = wordHash;
 }
 
-function isWordAlreadyInList(wordList, wordWithIds){
-  if(wordList.indexOf(wordWithIds)==-1){
-    return true;
+function displayWordCloud(){
+  var wordHash = calculatedWordsHash;
+  var hashKeys = Object.keys(wordHash);
+  var wordList = [];
+  for(var i=0; i<hashKeys.length; ++i){
+    var word = hashKeys[i];
+    var ids = wordHash[word];
+    wordList.push(new WordWithIds(word, ids));
   }
-  var isSimilar = false;
+  wordList.sort(function(word1, word2){
+    return word2.ids.size - word1.ids.size;
+  });
+  var trimmedWordList = [];
+  var ids = new Set();
   for(var i=0; i<wordList.length; ++i){
-    if(wordList[i].isWordSimilar(wordWithIds.word)){
-      wordList[i].addIds(wordWithIds.ids);
-      isSimilar = true;
+    if(wordList[i].hasOtherIds(ids)){
+      trimmedWordList.push(wordList[i]);
+      ids = new Set([...ids, ...wordList[i].ids]);
     }
   }
-  if(isSimilar){
+  console.log(ids.size);
+  console.log(trimmedWordList.length);
+  console.log("finished my part");
+
+  var fill = d3.scale.category20();
+
+  var words = trimmedWordList.map(function(word) {
+    var size =15 + word.ids.size / 10;
+    return {text: word.word.toUpperCase(), size: size};
+  });
+
+  d3.layout.cloud().size([960, 600])
+      .words(words)
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) *90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+  function draw(words) {
+    d3.select("body").append("svg")
+        .attr("width", 960)
+        .attr("height", 600)
+      .append("g")
+        .attr("transform", "translate(480,300)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+}
+
+function isSameWord(word1, word2){
+  if(word2.length - word1.length <= 2 && word1[0] === word2[0] && word1[1] === word2[1] && word1[2] === word2[2]){
     return true;
   }
   return false;
+
 }
 
 $(document).ready(function(){
@@ -66,6 +152,7 @@ $(document).ready(function(){
     });
     displayedVotesStack.push(votes);
     loadWords();
+    displayWordCloud();
   }).fail( function(d, textStatus, error) {
         console.error("getJSON failed, status: " + textStatus + ", error: "+error)
   });
--- opendata_app/js/classes/WordWithIds.js
+++ opendata_app/js/classes/WordWithIds.js
@@ -1,69 +1,28 @@
 // constructor
-function WordWithIds(word, ids){
+function WordWithIds(word, id){
   this.word = word;
-  if(Array.isArray(ids)){
-    this.ids = ids;
+  if(typeof id ==="number"){
+    this.ids = new Set();
+    this.ids.add(id);
   } else {
-    this.ids = [];
-    this.ids.push(ids);
+    this.ids = id;
   }
 }
 
-// adds one id
-WordWithIds.prototype.addId = function(additionalId){
-  if(this.ids.indexOf(additionalId)==-1){
-    this.ids.push(additionalId);
+// checks if word has special id
+WordWithIds.prototype.hasOtherIds = function(otherIds){
+  if(otherIds.size==0){
+    return true;
   }
-}
-
-// adds multiple ids
-WordWithIds.prototype.addIds = function(additionalIds){
-  for(var i=0; i<additionalIds.length; ++i){
-    this.addId(additionalIds[i]);
-  }
-}
-
-// returns how many ids are associated with this word
-WordWithIds.prototype.getCount = function(){
-  return this.ids.length;
-}
-
-/*
- * Checks, if two words are familiar and could be merged into each other.
- * If no second word is specified, use this word.
- */
-WordWithIds.prototype.isWordSimilar = function(wordToMerge, wordToMergeInto = this.word ){
-  var word1 = wordToMergeInto.toLowerCase();
-  var word2 = wordToMerge.toLowerCase();
-  if(new RegExp(word1).test(word2) && word2.length - word1.length <= 2 && word1.charAt(0) === word2.charAt(0) && word1.charAt(1) === word2.charAt(1) && word1.charAt(2) === word2.charAt(2)){
+  var hasSpecialId = false;
+  this.ids.forEach(function(id){
+    if(!otherIds.has(id)){
+      hasSpecialId = true;
+      return;
+    }
+  });
+  if(hasSpecialId){
     return true;
   }
   return false;
-
-}
-
-WordWithIds.prototype.compareWord = function(word, id){
-  var word1 = this.word.toLowerCase();
-  var word2 = word.toLowerCase();
-  if(word1.localeCompare(word2)==0){
-    this.addId(id);
-    return this;
-  }
-  if(new RegExp(word1).test(word2)){
-    this.addId(id);
-    if(this.isWordSimilar(word2, word1)){
-      return this;
-    } else {
-      return new WordWithIds(word, id);
-    }
-  }
-  if(new RegExp(word2).test(word1)){
-    if(this.isWordSimilar(word1, word2)){
-      this.word = word;
-      return this;
-    } else {
-      return new WordWithIds(word, this.ids).addId(id);
-    }
-  }
-  return null;
 }
