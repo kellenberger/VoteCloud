965febf825e8837c4b2609a09d2556a15918d6b2
skell94 <sven.kellenberger@kellenberger.tv>
2017-05-30 23:33:03 +0200

    added help

--- opendata_app/js/application.js
+++ opendata_app/js/application.js
@@ -46,27 +46,31 @@ $(document).ready(function(){
     $(".breadcrumb-wrapper").append("<a href=\"#!\" class=\"breadcrumb\">Details</a>");
     var voteId = $(this).find("input.vote-id").val();
     showVoteDetails(voteId);
-    $("#help-button").attr("href", "#scatterplot-help-modal");
-  });
+    $("#help-button").attr("href", "#scatterplot-help-modal").addClass("pulse");
+    setTimeout(
+      function() {
+        $("#help-button").removeClass("pulse");
+      }, 3000);
+    });
 
-  $("body").on("click", "g.legend", function(event){
-    if($(this).css("opacity")!=0.5){
-      $(this).css("opacity", 0.5);
-      $("circle."+this.id).hide();
-    } else {
-      $(this).css("opacity", 1);
-      $("circle."+this.id).show();
-    }
-  });
+    $("body").on("click", "g.legend", function(event){
+      if($(this).css("opacity")!=0.5){
+        $(this).css("opacity", 0.5);
+        $("circle."+this.id).hide();
+      } else {
+        $(this).css("opacity", 1);
+        $("circle."+this.id).show();
+      }
+    });
 
-  $("body").on("keypress", ".autocomplete-input", function(event){
-    if(event.keyCode == 13){
-      searchWord($(this).val());
-      $("ul.autocomplete-content").html("");
-    }
-  });
+    $("body").on("keypress", ".autocomplete-input", function(event){
+      if(event.keyCode == 13){
+        searchWord($(this).val());
+        $("ul.autocomplete-content").html("");
+      }
+    });
 
-  $("body").on("change", "input#legend-display", function(event){
-    $("g.legend").toggle(this.checked);
+    $("body").on("change", "input#legend-display", function(event){
+      $("g.legend").toggle(this.checked);
+    });
   });
-});
